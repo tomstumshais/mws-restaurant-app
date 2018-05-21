f2fceec6a7db31d65f345f1eab9753e179142677
Toms Tumshais <tomstumshais@gmail.com>
2018-05-21 23:47:58 +0300

    feat: update Gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', function () {
-    // place code for your default task here
+gulp.task('default', ['copy-html', 'copy-images'], function () {
+    gulp.watch('/index.html', ['copy-html']);
+    gulp.watch('./index.html').on('change', browserSync.reload);
+
+    browserSync.init({
+        server: './dist'
+    });
+});
+
+gulp.task('copy-html', function () {
+    gulp.src('./index.html')
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copy-images', function () {
+    gulp.src('img/*')
+        .pipe(gulp.dest('dist/img'));
 });
