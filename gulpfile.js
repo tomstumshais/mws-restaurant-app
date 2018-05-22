6ed306e796058d08064b04e97ffbc40f6123024c
Toms Tumshais <tomstumshais@gmail.com>
2018-05-22 23:22:12 +0300

    style: additional comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,25 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 
-gulp.task('default', ['copy-html', 'copy-images'], function () {
-    gulp.watch('/index.html', ['copy-html']);
-    gulp.watch('./index.html').on('change', browserSync.reload);
+gulp.task('default', ['copy-html', 'copy-images'], function() {
+  gulp.watch('/index.html', ['copy-html']);
+  gulp.watch('./index.html').on('change', browserSync.reload);
 
-    browserSync.init({
-        server: './dist'
-    });
+  browserSync.init({
+    server: './dist'
+  });
 });
 
-gulp.task('copy-html', function () {
-    gulp.src('./index.html')
-        .pipe(gulp.dest('./dist'));
+gulp.task('copy-html', function() {
+  gulp.src('./index.html').pipe(gulp.dest('./dist'));
 });
 
-gulp.task('copy-images', function () {
-    gulp.src('img/*')
-        .pipe(gulp.dest('dist/img'));
+gulp.task('copy-images', function() {
+  gulp.src('img/*').pipe(gulp.dest('dist/img'));
 });
+
+// var browserSync = require('browser-sync').create();
+//  browserSync.init({
+//      server: "./"
+//  });
+//  browserSync.stream();
